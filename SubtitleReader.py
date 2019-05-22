d2e0786afc1d7df9917f418b5cfe4c13cbf76f50
NicklasMCHD <NicklasMCHD@live.dk>
2019-05-22 17:46:50 +0200

    Added some more pre processing before ocr'ing the resulting image

--- SubtitleReader.py
+++ SubtitleReader.py
@@ -1,6 +1,7 @@
 import distutils
 import time
 import mss
+import cv2
 import numpy
 from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
@@ -15,12 +16,12 @@ def similar(a, b):
 
 output.output("Subtitle reader started. Now simply watch something with subtitles in full screen")
 with mss.mss() as sct:
-	#monitor = {'top': int(sct.monitors[0]["height"])-300, 'left': 200, 'width': int(sct.monitors[0]["width"]-200), 'height': int(sct.monitors[0]["height"]/2)}
 	monitor = {'top': int(sct.monitors[0]["height"]-250), 'left': 0, 'width': sct.monitors[0]["width"]-200, 'height': int(180)}
 	while True:
-		img_data = numpy.array(sct.grab(monitor))
-		im = Image.fromarray(img_data)
-		text = pytesseract.image_to_string(im, config="-l eng --psm 3")
+		img_data = cv2.cvtColor(numpy.array(sct.grab(monitor)), cv2.COLOR_RGB2BGR)
+		img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+		im.save("image.png")
+		text = pytesseract.image_to_string(img_data, lang="eng", config="--psm 6")
 		text = ''.join(e for e in text if e.isalnum() or e == " " or e == "," or e == ".")
 		if similar(old_text, text) < 0.5:
 			old_text = text
